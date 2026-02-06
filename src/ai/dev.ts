960a8192a178ed0ba2c584afaa93bf6b21fe10cb
Isaiah Smith <isaiah9238@gmail.com>
2026-02-06 01:50:34 +0000

    updated dev.ts

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,9 +2,10 @@ import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/analyze-code-snippet.ts';
-import '@/ai/flows/generate-initial-files.ts';
-import '@/ai/flows/filter-ai-output.ts';
-import '@/ai/flows/mentor-ai.ts';
-import '@/ai/flows/summarize-fetched-content.ts';
-import '@/ai/flows/integrate-lesson-plans.ts';
-import '@/ai/flows/filter-user-input.ts';
+import '@/ai/flows/link-genie.ts'
+//import '@/ai/flows/generate-initial-files.ts';
+//import '@/ai/flows/filter-ai-output.ts';
+//import '@/ai/flows/mentor-ai.ts';
+//import '@/ai/flows/summarize-fetched-content.ts';
+//import '@/ai/flows/integrate-lesson-plans.ts';
+//import '@/ai/flows/filter-user-input.ts';

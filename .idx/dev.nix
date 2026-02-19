d26c8f6ca2f0cc01aa148b4045705aa91ec61455
Isaiah Smith <isaiah9238@gmail.com>
2026-02-19 02:52:16 +0000

    dev.nix "hostname"

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -38,7 +38,7 @@
       enable = true; # <--- THE ONLY "ENABLE" YOU NEED
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
           manager = "web";
         };
       };

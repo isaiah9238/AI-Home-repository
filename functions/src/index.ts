1d67caf624529f81cc482370d3840ca6afe0ec5e
Isaiah Smith <isaiah9238@gmail.com>
2026-04-08 00:02:36 +0000

    Package-lock, index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,7 @@ import { googleAI } from "@genkit-ai/google-genai";
 import { onCallGenkit } from "firebase-functions/https";
 import { defineSecret } from "firebase-functions/params";
 import { setGlobalOptions } from "firebase-functions/v2";
-import { enableFirebaseTelemetry } from '@genkit-ai/firebase';
+import { enableFirebaseTelemetry } from "@genkit-ai/firebase";
 
 enableFirebaseTelemetry();
 

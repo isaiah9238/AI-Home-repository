526fd126f7566104565f2e26c1b5a68d6f7c69cb
Isaiah Smith <isaiah9238@gmail.com>
2026-04-09 04:31:00 +0000

    I commit

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,7 @@ const nextConfig: NextConfig = {
     serverActions: {
       allowedOrigins: [
         'localhost:3000',
+        '3000-firebase-studio-1770307012357.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev',
         '*.cloudworkstations.dev',
         '*.web.app',
         '*.firebaseapp.com',

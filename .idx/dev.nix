dcdc2a21d97dfce0bfd6c67d8753e551039be5a3
Isaiah Smith <isaiah9238@gmail.com>
2026-09-16 05:58:23 -0400

    i commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,34 +1,33 @@
 { pkgs, ... }: { 
   channel = "stable-24.05";
+
   packages = [
     pkgs.nodejs_22
-    pkgs.nodePackages.nodemon
     pkgs.psmisc
-    pkgs.sudo
-  ];
-  env = { };
-  idx = {
-    extensions = [
-      #any
     ];
+  
+  env = {
+    PORT = "3000";
+  };
+  
+  idx = {
+    extensions = [];
+
     previews = {
       enable = true;
       previews = {
         web = {
         command = [ "npm" "run" "dev" "--" "-p" "$PORT" "-H" "0.0.0.0" ];
         manager = "web";
-        env = {
-          PORT = "3000";
-          };
         };
       };
     };
+
     workspace = {
       onCreate = {
         npm-install = "npm install";
       };
-      onStart = {
-      };
+      onStart = {};
     };
   };
 }
